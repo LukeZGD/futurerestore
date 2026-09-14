63487e821378cd375ddc8fbeb9617a8cffd64dc5
LukeeGD <26163116+LukeZGD@users.noreply.github.com>
2026-09-14 13:49:32 +0800

    patch out irecv_init

--- compile.sh
+++ compile.sh
@@ -225,6 +225,8 @@ if [[ $OSTYPE == "linux"* ]]; then
     echo "Building futurerestore!"
     cd $FR_BASE
     cd ..
+    patch external/idevicerestore/src/dfu.c external/dfu.patch
+    patch external/idevicerestore/src/recovery.c external/recovery.patch
     ./autogen.sh $ALT_CONF_ARGS $CC_ARGS LDFLAGS="$LD_ARGS" LIBS="-llzma -lbz2 -lzstd -lcrypto -lz -ldl"
     make $JNUM
     mkdir -p bin/lib
